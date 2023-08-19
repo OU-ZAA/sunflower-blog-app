483869a1b179b4bdc05d9a8e03bc51b50b90f102
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2023-08-19 12:36:09 +0100

    Add shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import Logo from "../../public/logo.png";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      hello world
-    </main>
+    <>
+      <header>
+        <div>
+          <Image src={Logo} alt="App's logo" width={50} />
+        </div>
+      </header>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        hello world
+      </main>
+    </>
   );
 }

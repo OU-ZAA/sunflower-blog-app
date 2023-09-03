f81f3224b0f4b19dfa16e5c35362c455b0fb50bd
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2023-09-03 23:06:28 +0100

    Build api for getting posts

---
+++ src/lib/api.ts
@@ -0,0 +1,42 @@
+import fs from "fs";
+import { join } from "path";
+import matter from "gray-matter";
+
+const postsDirectory = join(process.cwd(), "_posts");
+
+function getPostBySlug(slug: string, fields: string[] = []) {
+  const realSlug = slug.replace(/\.md$/, "");
+  const fullPath = join(postsDirectory, slug);
+  const fileContents = fs.readFileSync(fullPath, "utf-8");
+  const { data, content } = matter(fileContents);
+
+  type Items = {
+    [key: string]: string;
+  };
+  const items: Items = {};
+
+  fields.forEach((field) => {
+    if (field === "slug") {
+      items[field] = realSlug;
+    }
+
+    if (field === "content") {
+      items[field] = content;
+    }
+
+    if (typeof data[field] !== "undefined") {
+      items[field] = data[field];
+    }
+  });
+
+  return items;
+}
+
+export function getAllPosts(fields: string[]) {
+  const slugs = fs.readdirSync(postsDirectory);
+  const posts = slugs
+    .map((slug) => getPostBySlug(slug, fields))
+    // Sort posts by date in descending order
+    .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
+  return posts;
+}

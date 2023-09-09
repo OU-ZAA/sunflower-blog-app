7a53367435252553449518b89451107f14e273cf
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2023-09-09 23:56:45 +0100

    remove author, cover image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,31 +24,10 @@ export default async function Home() {
         {posts.map((post) => (
           <Link href={`posts/${post.slug}`} key={post.slug}>
             <article className="mb-4 bg-white shadow rounded-md">
-              {post.coverImage && (
-                <Image
-                  src={post.coverImage}
-                  alt={`${post.title} blog cover`}
-                  width={700}
-                  height={300}
-                />
-              )}
               <div className="p-4">
-                <div className="flex gap-3 mb-4 items-center">
-                  <Image
-                    src={post.author.picture}
-                    alt={`${post.author.name} picture`}
-                    width={30}
-                    height={30}
-                    className="rounded-full border-2 border-slate-500"
-                  />
-                  <div>
-                    <p className="text-sm">{post.author.name}</p>
-                    <p className="text-xs">
-                      Posted on{" "}
-                      <span>{format(parseISO(post.date), "LLL d")}.</span>
-                    </p>
-                  </div>
-                </div>
+                <p className="text-xs">
+                  Posted on <span>{format(parseISO(post.date), "LLL d")}.</span>
+                </p>
                 <h1 className="text-2xl font-bold mb-2 hover:underline">
                   {post.title}
                 </h1>
--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -7,12 +7,7 @@ export type Post = {
   slug: string;
   title: string;
   excerpt: string;
-  coverImage: string;
   date: string;
-  author: {
-    picture: string;
-    name: string;
-  };
   content: string;
 };
 
@@ -31,42 +26,17 @@ export default async function Post({ params }: { params: { slug: string } }) {
   const content = await markdownToHtml(post.content || "");
 
   return (
-    <div className="bg-slate-100">
-      <div className="bg-white max-w-4xl mx-auto">
-        {post.coverImage ? (
-          <Image
-            src={post.coverImage}
-            alt={`${post.title} blog cover`}
-            width={900}
-            height={300}
-            className="mb-6"
-          />
-        ) : (
-          <div></div>
-        )}
-        <article className="p-8">
-          <div className="flex gap-3 mb-4 items-center">
-            <Image
-              src={post.author.picture}
-              alt={`${post.author.name} picture`}
-              width={50}
-              height={50}
-              className="rounded-full border-2 border-slate-500"
-            />
-            <div>
-              <p className="font-bold">{post.author.name}</p>
-              <p className="text-sm text-slate-400 font-semibold">
-                Posted on <span>{format(data, "LLL d")}.</span>
-              </p>
-            </div>
-          </div>
-          <h1 className="text-3xl font-bold mb-2">{post.title}</h1>
-          <article
-            className="prose"
-            dangerouslySetInnerHTML={{ __html: content }}
-          />
-        </article>
-      </div>
+    <div className="bg-slate-100 pt-8">
+      <article className="bg-white max-w-4xl mx-auto p-8 shadow rounded-lg">
+        <p className="text-sm mb-1">
+          Posted on <span>{format(data, "LLL d")}.</span>
+        </p>
+        <h1 className="text-3xl font-bold mb-2">{post.title}</h1>
+        <article
+          className="prose"
+          dangerouslySetInnerHTML={{ __html: content }}
+        />
+      </article>
     </div>
   );
 }
